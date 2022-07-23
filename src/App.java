74eb6da6891ae143411386566c3d65d787c3d241
Peterson dos Santos <peterson.opensource@hotmail.com>
2022-07-22 21:22:16 -0300

    AULA 04 - CRIANDO NOSSA PRÓPRIA API COM SPRING

--- src/App.java
+++ src/App.java
@@ -10,9 +10,12 @@ public class App {
         //String url = "https://api.mocki.io/v2/549a5d8b";
         //ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
           
-        String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        //String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
   
+        String url = "http://localhost:8080/linguagens";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
 
